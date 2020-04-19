54e0ac79f2414efa233341757d5dbcbf8c534fb8
pawanitzone <pawanitzone.kumar@gmail.com>
2020-04-19 17:50:33 +0530

    Update config.php

--- code/config.php
+++ code/config.php
@@ -1,11 +1,11 @@
 <?php
 /*
 // mysql_connect("database-host", "username", "password")
-$conn = mysql_connect("localhost","root","root") 
+//$conn = mysql_connect("localhost","root","root") 
 			or die("cannot connected");
 
 // mysql_select_db("database-name", "connection-link-identifier")
-@mysql_select_db("test",$conn);
+//@mysql_select_db("test",$conn);
 */
 
 /**
