3aa901cfbdd3766880913c9f08ac25d3c88980ff
pawanitzone <pawanitzone.kumar@gmail.com>
2020-04-20 13:45:13 +0530

    Update config.php

--- code/config.php
+++ code/config.php
@@ -6,18 +6,9 @@ $databaseUsername = getenv('DB_USERNAME');
 $databasePassword = getenv('DB_PASSWORD');
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    //you need to exit the script, if there is an error
-    exit();
-}
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
 }
 
 ?>
