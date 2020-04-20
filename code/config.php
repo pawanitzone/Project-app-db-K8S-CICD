24d8d6db279f4b9d68c0a7ac907b3aa10e2bed36
pawanitzone <pawanitzone.kumar@gmail.com>
2020-04-20 14:15:38 +0530

    Update config.php

--- code/config.php
+++ code/config.php
@@ -1,9 +1,14 @@
 <?php
 
-$databaseHost = getenv('DB_SERVER');
-$databaseName = getenv('DB_NAME');
-$databaseUsername = getenv('DB_USERNAME');
-$databasePassword = getenv('DB_PASSWORD');
+//$databaseHost = getenv('DB_SERVER');
+//$databaseName = getenv('DB_NAME');
+//$databaseUsername = getenv('DB_USERNAME');
+//$databasePassword = getenv('DB_PASSWORD');
+
+$databaseHost = 'db-service';
+$databaseName = 'crudwebdb';
+$databaseUsername = 'pawank';
+$databasePassword = '@pawan';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 if (mysqli_connect_errno()) {
