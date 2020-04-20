10e6b9c8d94210035ceadf6fb2d4892ff9eb8659
pawanitzone <pawanitzone.kumar@gmail.com>
2020-04-20 13:24:21 +0530

    Update config.php

--- code/config.php
+++ code/config.php
@@ -13,4 +13,11 @@ if (mysqli_connect_errno())
     exit();
 }
 
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
 ?>
